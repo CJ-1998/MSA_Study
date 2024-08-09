f2632af367cf3019be0bbafdb2832fac9b844184
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-08-09 15:43:09 +0900

    Feat : Product 서비스 설정
    Product 서비스 설정
    ProductApplication 및 Controller 수정 및 생성
    3개 어플리케이션 띄우기 위해 IDEA에서 복사 후 포트 다르게 해서 띄움

---
+++ com.spring-cloud.eureka.client.product/product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
@@ -0,0 +1,20 @@
+package com.spring_cloud.eureka.client.product;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
+    private String serverPort;
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id) {
+        return "Product " + id + " info!!!!! From port : " + serverPort;
+    }
+
+
+}
