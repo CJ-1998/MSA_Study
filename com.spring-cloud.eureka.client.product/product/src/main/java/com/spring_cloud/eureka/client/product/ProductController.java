df5ea9e48bedaa8307b364c7b03ae131eff76cda
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-08-10 23:04:06 +0900

    Feat : Config 서버 관련 구현
    Config 서버 생성 후 구현
    Product에 Config 서버로부터 yml 파일 받도록 구현 수정

--- com.spring-cloud.eureka.client.product/product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
+++ com.spring-cloud.eureka.client.product/product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
@@ -1,9 +1,11 @@
 package com.spring_cloud.eureka.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ProductController {
 
@@ -16,9 +18,18 @@ public class ProductController {
 //        return "Product " + id + " info!!!!! From port : " + serverPort;
 //    }
 
-    // api gateway 학습 위한 메서드
+//    // api gateway 학습 위한 메서드
+//    @GetMapping("/product")
+//    public String getProduct() {
+//        return "Product info!!!!! From port : " + serverPort;
+//    }
+
+    @Value("${message}")
+    private String message;
+
     @GetMapping("/product")
     public String getProduct() {
-        return "Product info!!!!! From port : " + serverPort;
+        return "Product detail from PORT : " + serverPort + " and message : " + this.message;
     }
+
 }
